0e729ed3cbb20231dc2282712f48c228eedbaa61
Jmalone20 <jasmine.malone@patelhs.org>
2021-02-01 14:02:56 +0000

    add a if/else statement

--- Main.java
+++ Main.java
@@ -1,16 +1,40 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) 
   {
+   Scanner scan = new Scanner(System.in);
 
-  int jersey = jerseyNumber(17, 9);
-  System.out.println("Your jersey number is " + jersey);
+   System.out.println("I'm here to try out for the volleyball team");
+
+   System.out.println("Whats your name ?");
+   String name = scan.next();
+
+   System.out.println("Whats your favourite number ?");
+   int favNum = scan.nextInt();
+   System.out.println("Whats your age ?");
+   int age = scan.nextInt(); 
+
+   System.out.println(sportQualifications(name));
+
+
+   int jersey = jerseyNumber(favNum, age);
+   System.out.println("Your jersey number is " + jersey);
 
   }
   // create 2 methods at least one parameter and it must return a value
 
-  static int jerseyNumber(int num1 , int num2)
+  static String sportQualifications(String name)
   {
-    int jersey = num1 - num2;
-  return jersey; 
+    if( height < 5.7 ){
+     System.out.println("Sorry, You are not tall enough");
+    }else{
+    System.out.println("Welcome to the team " + name + "!");
+    }
+  }
+
+  static int jerseyNumber(int favNum , int age)
+  {
+    int jersey = favNum - age;
+    return jersey; 
   }
 }
