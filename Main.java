ae727f3ccff3fb4757420a03d66b244af7156845
Jmalone20 <jasmine.malone@patelhs.org>
2021-02-01 13:15:05 +0000

    created my first method and gave it two parameters and returned it's value

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
-  class Main {
+class Main {
   public static void main(String[] args) 
   {
-   
-  }
 
+  int jersey = jerseyNumber(17, 9);
+  System.out.println("Your jersey number is " + jersey);
+
+  }
+  // create 2 methods at least one parameter and it must return a value
+
+  static int jerseyNumber(int num1 , int num2)
+  {
+    int jersey = num1 - num2;
+  return jersey; 
+  }
 }
